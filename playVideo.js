d4413353fcd61ac9e0119b3d96ce74a54345bfbb
Theja_Roop <nagisettythejaroop@gmail.com>
2024-05-07 14:02:11 -0400

    Add files via upload

---
+++ playVideo.js
@@ -0,0 +1,25 @@
+const video = document.getElementById("myVideo");
+
+// Play the video on button click (optional)
+// You can replace this with any event listener
+
+// function playVideo() {
+//   video.play();
+// }
+
+// Pause the video
+function pauseVideo() {
+  video.pause();
+}
+
+// Mute the video
+function muteVideo() {
+  video.muted = true;
+}
+
+// Unmute the video
+function unmuteVideo() {
+  video.muted = false;
+}
+
+// You can add more functionalities like setting volume, seeking position etc.
